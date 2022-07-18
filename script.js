e786816ef3232042da63463bbee9592e71be92d5
graycodesnu <graysonharvey@graysons-mbp.lan>
2022-07-17 21:10:50 -0500

    working on javascript - saving new events to local storage via the save button.

--- script.js
+++ script.js
@@ -2,6 +2,29 @@
 var currentDay = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentDay);
 
-// timeblocks
-var hourBlock = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#hourBlock").text(hourBlock);
+// Color-code time blocks relative to current time 
+
+// Save logged event to local storage
+    // Read text input 
+    // Link save button to local storage
+
+// Create input variable 
+var newEvent = document.querySelector ("#new-event");
+// Create button variable 
+var saveButton = document.querySelector("#save-btn");
+// Create event listener 
+saveButton.addEventListener("click" function(event) {
+    event.preventDefault();
+    // Create saved input var
+    var savedEvent = {
+        input: newEvent.value.trim()
+    };
+    // Save to local storage
+    localStorage.setItem("savedEvent", JSON.stringify(savedEvent));
+});
+
+    
+
+// Refresh page and saved event persists
+    // **I think this would be done with: init()**
+init()
